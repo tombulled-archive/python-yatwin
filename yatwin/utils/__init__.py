5ee183a473a87d66733b0e0ea8d1bec670bfb8e8
tombulled <26026015+tombulled@users.noreply.github.com>
2018-10-28 17:28:46 +0000

    Added chunk()

--- yatwin/utils/__init__.py
+++ yatwin/utils/__init__.py
@@ -4,7 +4,9 @@ Library containing utility functions for 'yatwin'
 Imports:
     .disect_url.disect_url
     .scan_port.scan_port
+    .chunks.chunk
 """
 
 from .disect_url import disect_url
 from .scan_port import scan_port
+from .chunk import chunk
