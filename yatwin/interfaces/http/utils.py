daa45e0b537faeb65777dd06189875ea979bbcaf
tombulled <26026015+tombulled@users.noreply.github.com>
2018-10-29 20:11:41 +0000

    Formatted jstopy nicer

--- yatwin/interfaces/http/utils.py
+++ yatwin/interfaces/http/utils.py
@@ -23,15 +23,22 @@ def jstopy(http_resp):
 
     for line in x.split(';'):
         line = line.strip()
+
         if line.lower().startswith('var'):
             line = line[3:].strip()
+
         if not line:
             continue
+
         eq_index = line.index('=')
+
         key, val = line[:eq_index], line[eq_index+1:]
+
         key = key.strip()
         val = val.strip()
+
         plus = False
+
         if key.endswith('+'):
             plus = True
             key = key[:-1].strip()
@@ -40,6 +47,7 @@ def jstopy(http_resp):
 
         if val.startswith('"') or val.startswith('\''):
             val = val[1:]
+
             if val.endswith('"') or val.endswith('\''):
                 val = val[:-1]
         elif 'array' not in str(val).lower():
@@ -51,6 +59,7 @@ def jstopy(http_resp):
         if '[' in key:
             key, *sub_keys = key.split('[')
             key = key.strip()
+            
             sub_keys = [sk.strip().rstrip(']').strip() for sk in sub_keys]
 
         if sub_keys:
