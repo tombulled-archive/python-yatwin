ba929c896efaf45633d1d371eef62fd0e3ba87c9
tombulled <26026015+tombulled@users.noreply.github.com>
2018-11-01 09:06:44 +0000

    Created FILES

--- yatwin/interfaces/http/files/__init__.py
+++ yatwin/interfaces/http/files/__init__.py
@@ -1,6 +1,26 @@
 """
 Imports:
+    .firmware_files
     .firmware_files.*
+
+Constants defined here:
+    FILES - List of all files
+    file_libs - Tuple of file libraries
 """
 
+from . import firmware_files
 from .firmware_files import *
+
+file_libs = \
+(
+    firmware_files,
+)
+
+FILES = []
+
+for file_lib in file_libs:
+    for attribute in dir(file_lib):
+        if attribute.startswith('_') or not attribute.isupper():
+            continue
+
+        FILES.append(getattr(file_lib, attribute))
